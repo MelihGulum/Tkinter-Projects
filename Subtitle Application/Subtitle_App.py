140b4c45bc8bc884f42726308d8237761b21d724
Melih <81585804+MelihGulum@users.noreply.github.com>
2024-01-03 14:55:51 +0300

    Subtitle App v.2.0

--- Subtitle Application/Subtitle_App.py
+++ Subtitle Application/Subtitle_App.py
@@ -1,42 +1,60 @@
 from tkinter import *
 import time
 from tkinter import filedialog as fd
+from tkinter import messagebox
+
+# Text Cleaning
+import string
+import re
 
 
-class stopwatch(Frame):
+class StopWatch(Frame):
     def __init__(self, parent=None, **kw):
         Frame.__init__(self, parent, kw)
         self.startTime = 0.0
         self.nextTime= 0.0
         self.onRunning = 0
         self.timestr = StringVar()
-        self.MakeWidget()
+        self.make_widget()
         self.frame = Frame
 
 
-    def MakeWidget(self):
-        global  start_button, pause_button, reset_button
+    def make_widget(self):
+        global  start_button, pause_button, reset_button, quit_button, load_button, \
+                words_button, take_back, forward_button, text, timeText
         timeText = Label(self, textvariable = self.timestr, text='00:00:00.000', font=('Arial', 30))
-        self.SetTime(self.nextTime)
-        timeText.pack(pady = 2, padx = (0,250))
+        self.set_time(self.nextTime)
+        timeText.pack(pady = 2, padx = (0,500))
 
+        load_button = Button(self, text='load', height=1, width=7, font=('Arial', 10), command=self.load_srt)
+        load_button.place(x=245, y=15)
         take_back = Button(self, text='back', height=1, width=7, font=('Arial', 10), command=self.back)
-        take_back.place(x=375, y=15)
-        start_button = Button(self, text='start', height=1, width=7, font=('Arial', 10), command=self.Start)
-        start_button.place(x=440, y=15)
+        take_back.place(x=310, y=15)
+        start_button = Button(self, text='start', height=1, width=7, font=('Arial', 10), command=self.start)
+        start_button.place(x=375, y=15)
         forward_button = Button(self, text='forward',height=1, width=7, font=('Arial', 10), command=self.forward)
-        forward_button.place(x=505, y=15)
+        forward_button.place(x=440, y=15)
         pause_button = Button(self, text='pause', height=1, width=7, font=('Arial', 10), command=self.stop)
-        pause_button.place(x=570, y=15)
-        reset_button = Button(self, text='reset', height=1, width=7, font=('Arial', 10), command=self.Reset)
-        reset_button.place(x=635, y=15)
+        pause_button.place(x=505, y=15)
+        reset_button = Button(self, text='reset', height=1, width=7, font=('Arial', 10), command=self.reset)
+        reset_button.place(x=570, y=15)
+        words_button = Button(self, text='word', height=1, width=7, font=('Arial', 10), command=self.word_tokenize)
+        words_button.place(x=635, y=15)
         quit_button = Button(self, text='quit', height=1, width=7, font=('Arial', 10), command=quit)
         quit_button.place(x=700, y=15)
 
+        text = Text(self, height=12, width=100)
+        text.pack(fill=X, expand=NO, pady = 2, padx = 2)
+
+    def load_srt(self):
+        global texts
         import re
         def srt_to_dict(srtText):
             with open(srtText) as f:
                 srtText = f.read()
+                #Reduce multiple blank lines to single
+                srtText = re.sub(r'\n\s*\n', '\n\n', srtText)
+
             subs = []
             for s in re.sub('\r\n', '\n', srtText).split('\n\n'):
                 st = s.split('\n')
@@ -49,27 +67,21 @@ class stopwatch(Frame):
             with open('subtitle.txt', 'w',encoding="utf-8") as f:
                 for line in subs:
                     f.write(f"{line}\n")
-
             return subs
-
-        text = Text(self, height=12, width=100)
-        text.pack(fill=X, expand=NO, pady = 2, padx = 2)
 
         filetypes = (('srt files', '*.srt'),)
         f = fd.askopenfile(filetypes=filetypes)
         file = srt_to_dict(f.name)
 
-        started =[]
-        ended= []
+        started = []
+        ended = []
         texts = []
-        for i in range(len(file)):
-                    started.append(file[i][0])
-                    ended.append(file[i][1])
-                    texts.append(file[i][2])
-                    start = file[i][0]
-                    end = file[i][1]
-                    texted = file[i][2]
-                    text.insert(END, f"{start} --> {end}, {texted}\n")
+        for idx, subs in enumerate(file):
+                    started.append(subs[0])
+                    ended.append(subs[1])
+                    texts.append(subs[2])
+                    text.insert(END, f"{subs[0]} --> {subs[1]}, {subs[2]}\n")
+
         content_text = Text(self, height=1, width=100)
         content_text.pack(fill=X, expand=NO, pady = 2, padx = 2)
         content_text.config(state="disabled")
@@ -80,14 +92,14 @@ class stopwatch(Frame):
                 if(format(self.timestr.get()) in started[i]):
                     text.pack_forget()
                     app.geometry("800x90")
-                    app.attributes("-transparentcolor",app['bg'])
+                    app.attributes("-transparentcolor", app['bg'])
+
+                    widget_config_list = [start_button, pause_button, reset_button, quit_button,
+                                          take_back, forward_button, words_button, load_button]
+                    for widget in widget_config_list:
+                        widget.config(bg="white")
+
                     timeText.config(fg="white")
-                    start_button.config(bg="white")
-                    pause_button.config(bg="white")
-                    reset_button.config(bg="white")
-                    quit_button.config(bg="white")
-                    take_back.config(bg="white")
-                    forward_button.config(bg="white")
                     content_text.config(state="normal")
                     content_text.config(height=3,font=("Segoe UI",14,"bold"), fg="black")
                     app.attributes("-alpha",0.3)
@@ -100,44 +112,93 @@ class stopwatch(Frame):
                         app.destroy()
                 else:
                     pass
+
         self.timestr.trace_add('write', callback)
+        load_button['state'] = 'disabled'
 
-    def Updater(self):
+    def word_tokenize(self):
+        try:
+            def decontracted(phrase):
+                # specific
+                phrase = re.sub(r"won\'t", "will not", phrase)
+                phrase = re.sub(r"can\'t", "can not", phrase)
+                phrase = re.sub(r"- ", "", phrase)
+
+                # general
+                phrase = re.sub(r"n\'t", " not", phrase)
+                phrase = re.sub(r"\'re", " are", phrase)
+                phrase = re.sub(r"\'s", " is", phrase)
+                phrase = re.sub(r"\'d", " would", phrase)
+                phrase = re.sub(r"\'ll", " will", phrase)
+                phrase = re.sub(r"\'t", " not", phrase)
+                phrase = re.sub(r"\'ve", " have", phrase)
+                phrase = re.sub(r"\'m", " am", phrase)
+                return phrase
+
+            def clean_text(sent):
+                sent = decontracted(sent)
+                sent = sent[0].lower() + sent[1:]
+                sent = sent.translate(str.maketrans('', '', string.punctuation)).split(" ")
+                return sent
+
+            tokens = [clean_text(sent) for sent in texts]
+            ordered_tokens = set()
+            result = []
+            for words in tokens:
+                for word in words:
+                    if word not in ordered_tokens:
+                        ordered_tokens.add(word)
+                        result.append(word)
+
+            with open('words.txt', 'w', encoding="utf-8") as f:
+                for word in result:
+                    f.write(f"{word}\n")
+            messagebox.showinfo("Subtitle_App", "Tokenizing Done!!!\nYou can find it in 'words.txt'.")
+        except:
+            messagebox.showwarning("Subtitle_App", "Can not tokenize")
+
+
+    def updater(self):
         self.nextTime = time.time() - self.startTime
-        self.SetTime(self.nextTime)
-        self.timer = self.after(50, self.Updater)
+        self.set_time(self.nextTime)
+        self.timer = self.after(50, self.updater)
 
-    def Start(self):
+
+    def start(self):
         if not self.onRunning:
             self.startTime = time.time() - self.nextTime
-            self.Updater()
+            self.updater()
             self.onRunning = 1
             start_button['state'] = 'disabled'
             pause_button['state'] = 'normal'
             reset_button['state'] = 'normal'
 
+
     def stop(self):
         if self.onRunning:
             self.after_cancel(self.timer)
             self.nextTime = time.time() - self.startTime
-            self.SetTime(self.nextTime)
+            self.set_time(self.nextTime)
             self.onRunning = 0
             start_button['state'] = 'normal'
             pause_button['state'] = 'disabled'
             reset_button['state'] = 'normal'
 
-    def Reset(self):
+
+    def reset(self):
         start_button['state'] = 'normal'
         pause_button['state'] = 'normal'
         reset_button['state'] = 'normal'
         self.startTime = time.time()
         self.nextTime = 0.0
-        self.SetTime(self.nextTime)
+        self.set_time(self.nextTime)
 
-    def SetTime(self, nextElap):
+
+    def set_time(self, nextElap):
         hours, rem = divmod(nextElap, 3600)
         minutes, seconds = divmod(rem, 60)
         self.timestr.set("{:0>2}:{:0>2}:{:02.0f}".format(int(hours),int(minutes),seconds))
+
 
     def back(self):
             if (self.nextTime - 5.0) <= 0:
@@ -145,29 +206,30 @@ class stopwatch(Frame):
             else:
                 self.after_cancel(self.timer)
                 self.nextTime = self.nextTime - 5.0
-                self.SetTime(self.nextTime)
+                self.set_time(self.nextTime)
                 self.onRunning = 0
                 start_button['state'] = 'normal'
                 pause_button['state'] = 'disabled'
                 reset_button['state'] = 'normal'
 
+
     def forward(self):
         self.after_cancel(self.timer)
         self.nextTime = self.nextTime + 5.0
-        self.SetTime(self.nextTime)
+        self.set_time(self.nextTime)
         self.onRunning = 0
         start_button['state'] = 'normal'
         pause_button['state'] = 'disabled'
         reset_button['state'] = 'normal'
 
+
 if __name__ == '__main__':
     app = Tk()
-    StopWatch = stopwatch(app)
+    StopWatch = StopWatch(app)
     StopWatch.pack(side=TOP)
     app.title("Subtitle Application")
-    app_height = 400
+    app_height = 300
     app_width = 850
     app.geometry(f'{app_width}x{app_height}+{400}+{80}')
     app.resizable(False, False)
     app.mainloop()
-
